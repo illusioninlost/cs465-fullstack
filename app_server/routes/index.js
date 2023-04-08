fc78d9e744e607f4de54a0b182b0e1c867f32cb9
Richard Chang <rchang9999@yahoo.com>
2023-04-07 22:07:20 -0400

    change controller index route

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 const controller= require('../controllers/main')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', controller.index);
+
 
 module.exports = router;
